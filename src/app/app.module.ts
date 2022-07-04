52e675ea9585c8d9b3e766b05bad56a3c115bb9c
KirillGordineRhenag <kirill.gordine@rhenag.de>
2022-07-04 13:55:30 +0200

    removed default component and added styling to pre tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { MyControlWithErrorMessagesComponent } from './my-control-with-error-messages/my-control-with-error-messages.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    MyControlWithErrorMessagesComponent,
-  ],
+  declarations: [AppComponent, MyControlWithErrorMessagesComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
